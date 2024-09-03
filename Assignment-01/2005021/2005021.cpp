83d608c763330a4482d82719dcf39f1a44584a05
Afzal Hossan <afzalhossan2005021@gmail.com>
2024-09-03 19:41:05 +0600

    Task-02: Determining the solvability of the input initial board state is added.

--- Assignment-01/2005021/2005021.cpp
+++ Assignment-01/2005021/2005021.cpp
@@ -20,4 +20,41 @@ int main() {
             }
         }
     }
+    vector<int> linearized_grid(k*k + 1);
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            linearized_grid[i*k + j + 1] = grid[i][j];
+        }
+    }
+    int inversions = 0;
+    for(int i = 1; i <= k*k; i++){
+        for(int j = i + 1; j <= k*k; j++){
+            if(linearized_grid[i] == -1 || linearized_grid[j] == -1){
+                continue;
+            }
+            if(linearized_grid[i] > linearized_grid[j]){
+                inversions++;
+            }
+        }
+    }
+    bool is_solvable;
+    if(k & 1){  // size k is an odd integer
+        if(inversions & 1){
+            is_solvable = false;
+        } else {
+            is_solvable = true;
+        }
+    } else {    // size k is an even integer
+        int blank_row_from_bottom = k - blank_row;  // blank row is counted from bottom && 1-indexed
+        if((inversions + blank_row_from_bottom) & 1){
+            is_solvable = true;
+        } else {
+            is_solvable = false;
+        }
+    }
+    if(is_solvable){
+        cout << "The given board state is solvable" << endl;
+    } else {
+        cout << "The given board state is not Solvable" << endl;
+    }
 }
